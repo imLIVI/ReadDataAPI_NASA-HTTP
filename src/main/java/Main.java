db7a2411fd86718565059f7dd48038703b5496c4
imLIVI <lesly120898@gmail.com>
2022-11-03 23:06:54 +0300

    Implement the app to reading data from Nasa

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URI =
+            "https://api.nasa.gov/planetary/apod?api_key=5XQOF8x0Eu3M2cNgaSNpdu3Vh0rQvhEVsCBWfeyj";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        //создание объекта запроса
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        //отправка запроса
+        CloseableHttpResponse response = httpClient.execute(request);
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        //преобразование json в java
+        mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+        NasaResponse info = mapper.readValue(response.getEntity().getContent(),
+                new TypeReference<NasaResponse>() {
+                });
+
+        //получение имени файла
+        String[] url = info.getUrl().split("/");
+        String nameFile = url[url.length - 1];
+        //сохранение картинки в файл
+        try (BufferedInputStream in = new BufferedInputStream(new URL(info.getUrl()).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(nameFile)) {
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+}
