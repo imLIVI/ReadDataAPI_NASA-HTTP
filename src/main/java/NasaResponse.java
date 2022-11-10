bbb8a549631eeb5a317f33824cd291c3a8a28b68
imLIVI <lesly120898@gmail.com>
2022-11-10 15:28:22 +0300

    Brought variable names to the standard 2

--- src/main/java/NasaResponse.java
+++ src/main/java/NasaResponse.java
@@ -15,16 +15,16 @@ public class NasaResponse {
             @JsonProperty("data") String date,
             @JsonProperty("explanation") String explanation,
             @JsonProperty("hdurl") String hdurl,
-            @JsonProperty("media_type") String media_type,
-            @JsonProperty("service_version") String service_version,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
             @JsonProperty("title") String title,
             @JsonProperty("url") String url) {
         this.copyright = copyright;
         this.date = date;
         this.explanation = explanation;
         this.hdurl = hdurl;
-        this.mediaType = media_type;
-        this.serviceVersion = service_version;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
